b02f78028592f3f77ab30a79f947ef13992ad126
Zupanico <50524719+Zupanico@users.noreply.github.com>
2023-10-19 13:42:10 -0400

    first commit

---
+++ src/main.ino
@@ -0,0 +1,33 @@
+/*
+Projet: Defi du combattant
+Equipe: 
+Auteurs: Evan Frappier, Nicolas Garant, Félix Bordeleau et Kanvali Bakayoko
+Description: Permet de guider le robot dans le labyrinthe
+Date: 09/10/2023
+*/
+
+#include <LibRobus.h> // Essentielle pour utiliser RobUS
+
+// Variables globales et defines
+// ============================
+
+// ============================
+
+// Fonctions
+// ============================
+
+// Fonctions d'initialisation (setup)
+
+void setup()
+{
+
+}
+
+// Fonctions de boucle infini (loop())
+
+void loop()
+{
+
+}
+// Fin du programme
+// ============================
